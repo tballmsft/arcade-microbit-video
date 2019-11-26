5005f1a82e8a62b6f3b56691d14ce1dac55ca441
Tom Ball <tballmsft@users.noreply.github.com>
2019-11-26 11:13:06 -0800

    add lyrics

--- main.ts
+++ main.ts
@@ -1,6 +1,113 @@
 // 2 bar drum intro
  drumIntro()
 
-// "I want my micro:bit"
+// (verse 1a)
 
-// "I want my micro:bit"
+// I want my micro:bit"
+
+// I want my micro:bit"
+
+// She has a bit in her pocket,
+
+// He put a bit on his hat,
+
+// They left bits in the classroom,
+
+// What will the teachers make of that?
+
+
+// (verse 1b)
+
+// Juliet sent a radio message,
+
+// "I miss you, Romeo".
+
+// He saw his future decoded,
+
+// In the matrix's fiery glow.
+
+
+// (chorus 1a)
+
+// Accelerate, Educate, Activate,
+
+// Go ahead and push the buttons.
+
+// Take it, shake it, 
+
+// Break it if you can.
+
+
+// (chorus 1b)
+
+// JST, USB, SPI, BLE, I2C
+
+// No incommunicado.
+
+// We’re nerding out on digital technology.
+
+// (Guitar solo - eight bars)
+
+
+// (verse 2a)
+
+// Everyone, get with the program,
+
+// It’s fun to make and code,
+
+// In school and at the playground,
+
+// Even when you’re on the road.
+
+
+// (verse 2b)
+
+// The BBC, they had the vision:
+
+// A micro: bit for every Year Seven.
+
+// With 30 partners round the table,
+
+// How could this gig not be heaven?
+
+
+// (chorus 2a)
+
+// Three Volts and Ground,
+
+// May be found - on the edge,
+
+// Just one step to a short circuit.
+
+// Who knew that the Beebs liked to walk the high- wire ?
+
+
+// (chorus 2b)
+
+// Lancaster, Nordic, ARM, Microsoft,
+
+// Worked their charm -- overtime 
+
+// And don’t forget Farnell
+
+// One million micro: bits for the UK
+
+// One million micro: bits for the UK
+
+// One million micro: bits for the UK
+
+
+// I want my micro: bit
+
+// I want my micro: bit
+
+// 4 measures
+
+// 4 measures
+
+// I got a micro: bit
+// What can you do with it?
+// I’m gonna make and code
+// And strike the motherlode.
+
+// repeated
